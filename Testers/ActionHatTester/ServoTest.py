66a9e3d3ee8a9f1bfbf269ca65f05f2dd4b62f48
frank-qcd-qk <cxq41@case.edu>
2018-10-13 19:23:47 -0400

    Trying to fix servo tester and understand servo tester
    Former-commit-id: eab48f2ab136967e931f4a59b6f69d59b502d1fe

--- Testers/ActionHatTester/ServoTest.py
+++ Testers/ActionHatTester/ServoTest.py
@@ -11,21 +11,26 @@ servo_max = 600  # Max pulse length out of 4096
 # Helper function to make setting a servo pulse width simpler.
 def set_servo_pulse(channel, pulse):
     pulse_length = 1000000    # 1,000,000 us per second
-    pulse_length //= 60       # 60 Hz
+    pulse_length /= 50       # 50 Hz
     print('{0}us per period'.format(pulse_length))
-    pulse_length //= 4096     # 12 bits of resolution
+    pulse_length /= 4096     # 12 bits of resolution
     print('{0}us per bit'.format(pulse_length))
     pulse *= 1000
-    pulse //= pulse_length
+    pulse /= pulse_length
     pwm.setPWM(channel, 0, pulse)
 
-# Set frequency to 60hz, good for servos.
-pwm.setPWMFreq(60)
+# Set frequency to 50hz, good for servos.
+pwm.setPWMFreq(50)
 
 print('Moving servo on channel 0, press Ctrl-C to quit...')
-while True:
+i = 0;
+while (i<2):
     # Move servo on channel O between extremes.
     pwm.setPWM(0, 0, servo_min)
     time.sleep(1)
     pwm.setPWM(0, 0, servo_max)
     time.sleep(1)
+    print (i)
+    i+=1
+
+pwm.setPWM(0,0,350)
